0e08197ada89779d8571e35fd161d5a6e4263324
Schneider Werner-Walter <wschneider@developmentgateway.org>
2015-02-13 15:00:23 +0200

    Update epel_repo_status facter.

--- puppet/modules/happydev/lib/facter/epel_repo_status.rb
+++ puppet/modules/happydev/lib/facter/epel_repo_status.rb
@@ -5,7 +5,7 @@ Facter.add('epel_repo_status') do
   setcode do
     if File.exist?('/etc/yum.repos.d/epel.repo')
       # @TODO: Find a better way to determine the status of the EPEL repository.
-      cmd = "yum repolist all | grep '^\*\?epel\s\+' | sed 's/.*enabled:.*/repo-is-enabled/'"
+      cmd = "yum repolist all | grep '^epel ' | sed 's/.*enabled:.*/repo-is-enabled/'"
       status = Facter::Util::Resolution.exec(cmd)
       if (status == 'repo-is-enabled')
         'enabled'

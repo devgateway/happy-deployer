61f391a35a48f071a528f08d85b84344be4ed53d
Schneider Werner-Walter <wschneider@developmentgateway.org>
2016-03-29 01:27:59 +0300

    Add SL7.2 veewee template.

--- veewee/scientificlinux-6.5-x86_64-netboot/epel.sh
+++
@@ -1,8 +0,0 @@
-# Install EPEL Repository.
-
-# Download and install the repository.
-rpm -Uvh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
-
-# Import the public key.
-gpg --import /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-6
-
--- veewee/scientificlinux-7.2-x86_64-netinst/definition.rb
+++ veewee/scientificlinux-7.2-x86_64-netinst/definition.rb
@@ -6,17 +6,17 @@ Veewee::Session.declare({
   :hostiocache => 'off',
   :os_type_id => 'RedHat_64',
   :iso_file => "SL-7.1-x86_64-netinst.iso",
-  :iso_src => "http://ftp.heanet.ie/pub/rsync.scientificlinux.org/7x/x86_64/iso/SL-7.1-x86_64-netinst.iso",
-  :iso_shai => "2e69c4b36aae046963e06b67b40cd41f7b1b9a14",
-  :iso_download_timeout => 1000,
-  :boot_wait => "15",
+  :iso_src => "http://ftp.heanet.ie/pub/rsync.scientificlinux.org/7.2/x86_64/iso/SL-7.2-x86_64-netinst.iso",
+  :iso_shai => "7983b8dd597478cedf0a0f05294d35221bffddb7",
+  :iso_download_timeout => 1200,
+  :boot_wait => "16",
   :boot_cmd_sequence => [
     '<Tab> text ks=http://%IP%:%PORT%/ks.cfg<Enter>'
   ],
   :kickstart_port => "7122",
   :kickstart_timeout => 300,
   :kickstart_file => "ks.cfg",
-  :ssh_login_timeout => "10000",
+  :ssh_login_timeout => "10800",
   :ssh_user => "vagrant",
   :ssh_password => "vagrant",
   :ssh_key => "",
@@ -26,17 +26,13 @@ Veewee::Session.declare({
   :shutdown_cmd => "/sbin/halt -h -p",
   :postinstall_files => [
     "base.sh",
-    # "ruby.sh",
-    # "chef.sh",
-    # "puppet.sh",
-    "epel.sh",
-    "ansible.sh",
     "vagrant.sh",
+    "repo-epel.sh",
+    "repo-ius.sh",
+    "ansible.sh",
     "virtualbox.sh",
-    #"kvm.sh",
-    #"vmfusion.sh",
     "cleanup.sh",
     "zerodisk.sh"
   ],
-  :postinstall_timeout => 10000
+  :postinstall_timeout => 10800
 })
